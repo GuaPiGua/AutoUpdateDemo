26febeb2d67297bc7b25399639a28cfa62e8725e
GuaPi <1123655804@qq.com>
2022-02-07 23:02:09 +0800

    首次提交添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace AutoUpdateDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AutoUpdater.Start("https://tmw-1255523279.cos.ap-shanghai.myqcloud.com/test.xml");
+            AutoUpdater.ApplicationExitEvent += delegate
+            {
+                System.Windows.MessageBox.Show("12312313");
+                System.Windows.Application.Current.Shutdown();
+            };
+            AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
+            
+            //AutoUpdater.CheckForUpdateEvent += Class1.AutoUpdaterOnCheckForUpdateEvent;
+
+
+
+        }
+
+       public void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
+        {
+            
+            if (AutoUpdater.DownloadUpdate(args))
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+    }
+}
